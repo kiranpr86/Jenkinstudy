50ab636f812fecba6bfeac03b80d0814cf4080bc
KPtester <connectkiran18@gmail.com>
2024-02-22 13:29:37 +0000

    First commit for Jenkin Study

---
+++ Jenkinstudy.Automation/src/test/java/fortesting/Jenkinclass2.java
@@ -0,0 +1,12 @@
+package fortesting;
+import org.testng.annotations.Test;
+
+public class Jenkinclass2 {
+	
+	@Test
+	public void classforjenkumon()
+	{
+		System.out.println("My name is Jenkumon");
+	}
+
+}
---
+++ Jenkinstudy.Automation/src/test/java/fortesting/Jenkinsclass1.java
@@ -0,0 +1,12 @@
+package fortesting;
+import org.testng.annotations.Test;
+
+public class Jenkinsclass1 {
+	
+	@Test
+	public void classforjenku()
+	{
+		System.out.println("My name is jenku");
+	}
+
+}
