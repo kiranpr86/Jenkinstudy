ad5638f3763da0560a742bcd3f635c1e74425dbd
kptester <connectkiran18@gmail.com>
2024-02-23 10:29:21 +0000

    Jenkumol commit

---
+++ Jenkinstudy.Automation/src/test/java/fortesting/Jenkinsclass3.java
@@ -0,0 +1,12 @@
+package fortesting;
+import org.testng.annotations.Test;
+
+public class Jenkinsclass3 {
+	
+	@Test
+	public void classforjenku()
+	{
+		System.out.println("My name is jenkumol");
+	}
+
+}
